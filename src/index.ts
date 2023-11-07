212079d7418ab6eff360f258ef620030e240f504
Yepqwq <yepqwq@shijunyoudeMacBook-Air.local>
2023-11-07 13:43:38 +0800

    N/A

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,6 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.command('echo <message>')
+    .action((_, message) => message)
 }
